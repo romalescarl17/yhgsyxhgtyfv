76713a0da4ef9db0fd4051e031000888f23ea116
romalescarl17 <carllouiseromales15@gmail.com>
2024-12-16 11:35:19 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,8 @@
 <footer class="footer footer-center bg-base-300 text-base-content p-4 sticky top-[100vh]">
 	<aside>
 		<p>
-			<a href="https://github.com/Bullrich/GitHub-Status-Page">GitHub-Status-Page</a> made with ❤️
-			by <a href="https://bullrich.dev">Bullrich</a>
+			<a href="https://sampfreeph.live/discord">SampFreePH</a> hosted made with ❤️
+			by <a href="https://sampfreeph.live/discord">notcarlrdev</a>
 		</p>
 	</aside>
 </footer>
